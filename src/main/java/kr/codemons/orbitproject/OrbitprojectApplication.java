e1f7634fde826d3bd51e8ea3a5e3582812a5cb59
조영준 <joyoungjun8590@hotmail.com>
2023-06-03 18:46:54 +0900

    [#1] 초기화

---
+++ src/main/java/kr/codemons/orbitproject/OrbitprojectApplication.java
@@ -0,0 +1,13 @@
+package kr.codemons.orbitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitprojectApplication.class, args);
+	}
+
+}
