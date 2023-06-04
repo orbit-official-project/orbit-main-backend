9c89dca8bc714152e865ba066e6ea1f24a9bd3c8
조영준 <joyoungjun8590@hotmail.com>
2023-06-04 18:58:56 +0900

    [#2] 유저 인증 기반 작업

--- src/main/java/kr/codemons/orbitproject/web/config/MailBeanConfig.java
+++ src/main/java/kr/codemons/orbitproject/web/config/MailBeanConfig.java
@@ -1,5 +1,6 @@
 package kr.codemons.orbitproject.web.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@Slf4j
 @Configuration
 public class MailBeanConfig {
 
