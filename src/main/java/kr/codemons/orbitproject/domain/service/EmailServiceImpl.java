e0812fcb51fb340c05a04d8dd29ae95b818ac22d
조영준 <joyoungjun8590@hotmail.com>
2023-06-04 20:05:11 +0900

    EmailService 기능 및 테스트 완료

--- src/main/java/kr/codemons/orbitproject/domain/service/EmailServiceImpl.java
+++ src/main/java/kr/codemons/orbitproject/domain/service/EmailServiceImpl.java
@@ -1,17 +1,46 @@
 package kr.codemons.orbitproject.domain.service;
 
+import jakarta.mail.internet.MimeMessage;
+import kr.codemons.orbitproject.domain.exception.InitMessageHelperException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    @Override
-    public void sendCertificationMail(String email) {
-        
-    }
+    private final JavaMailSender javaMailSender;
+
+    @Value("${smtp.email}") private String smtpEmail;
 
     @Override
-    public String createCode(String seed) {
-        return null;
+    public void sendCertificationMail (String email)  {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = initializeMimeMessageHelper(message, email);
+
+        javaMailSender.send(message);
+    }
+
+    private String createCode (String seed) {
+        return "testCode";
+    }
+
+    private MimeMessageHelper initializeMimeMessageHelper (MimeMessage mimeMessage, String setTo) {
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+        try {
+            helper.setFrom(smtpEmail);
+            helper.setTo(setTo);
+            helper.setSubject("test subject");
+            helper.setText(createCode("dummyText"));
+        }
+        catch (Exception e) {
+            throw new InitMessageHelperException();
+        }
+
+        return helper;
     }
 }
